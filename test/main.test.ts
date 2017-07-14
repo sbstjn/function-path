bff2a22b75900e326cc151c00b55992b17eb2b8a
sbstjn <mail@sbstjn.com>
2017-07-14 13:52:11 +0200

    Update tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,24 +1,24 @@
-import Path from '../src/main.ts'
+import fp from '../src/main'
 
 it('parse file from path', () => {
-  expect(Path.file('foo.bar/baz.lorem')).toBe('foo.bar/baz.js')
-  expect(Path.file('foo/bar.baz/lorem')).toBe('foo/bar.baz/lorem.js')
-  expect(Path.file('foo/bar.baz/lorem.ipsum')).toBe('foo/bar.baz/lorem.js')
-  expect(Path.file('foo/bar/baz.lorem')).toBe('foo/bar/baz.js')
-  expect(Path.file('foo/bar.baz.lorem')).toBe('foo/bar.js')
-  expect(Path.file('foo/bar.baz')).toBe('foo/bar.js')
-  expect(Path.file('foo/bar')).toBe('foo/bar.js')
-  expect(Path.file('foo')).toBe('foo.js')
+  expect(fp.file('foo.bar/baz.lorem')).toBe('foo.bar/baz.js')
+  expect(fp.file('foo/bar.baz/lorem')).toBe('foo/bar.baz/lorem.js')
+  expect(fp.file('foo/bar.baz/lorem.ipsum')).toBe('foo/bar.baz/lorem.js')
+  expect(fp.file('foo/bar/baz.lorem')).toBe('foo/bar/baz.js')
+  expect(fp.file('foo/bar.baz.lorem')).toBe('foo/bar.js')
+  expect(fp.file('foo/bar.baz')).toBe('foo/bar.js')
+  expect(fp.file('foo/bar')).toBe('foo/bar.js')
+  expect(fp.file('foo')).toBe('foo.js')
 })
 
 it('parse path from path', () => {
-  expect(Path.path('foo/bar.baz/lorem/ipsum.dolor')).toBe('dolor')
-  expect(Path.path('foo/bar/baz/lorem.ipsum.dolor')).toBe('ipsum.dolor')
-  expect(Path.path('foo/bar.baz/lorem.ipsum/dolor.sit.amet')).toBe('sit.amet')
-  expect(Path.path('foo/bar.baz.lorem')).toBe('baz.lorem')
-  expect(Path.path('foo/bar.baz')).toBe('baz')
-  expect(Path.path('foo/bar')).toBe(null)
-  expect(Path.path('foo')).toBe(null)
+  expect(fp.path('foo/bar.baz/lorem/ipsum.dolor')).toBe('dolor')
+  expect(fp.path('foo/bar/baz/lorem.ipsum.dolor')).toBe('ipsum.dolor')
+  expect(fp.path('foo/bar.baz/lorem.ipsum/dolor.sit.amet')).toBe('sit.amet')
+  expect(fp.path('foo/bar.baz.lorem')).toBe('baz.lorem')
+  expect(fp.path('foo/bar.baz')).toBe('baz')
+  expect(fp.path('foo/bar')).toBe(null)
+  expect(fp.path('foo')).toBe(null)
 })
 
 it('has path in object', () => {
@@ -30,13 +30,13 @@ it('has path in object', () => {
     }
   }
 
-  expect(Path.has(data, 'first')).toBeTruthy()
-  expect(Path.has(data, 'first.second')).toBeTruthy()
-  expect(Path.has(data, 'first.second.last')).toBeTruthy()
+  expect(fp.has(data, 'first')).toBeTruthy()
+  expect(fp.has(data, 'first.second')).toBeTruthy()
+  expect(fp.has(data, 'first.second.last')).toBeTruthy()
 
-  expect(Path.has(data, 'second')).toBeFalsy()
-  expect(Path.has(data, 'first.last')).toBeFalsy()
-  expect(Path.has(data, 'first.second.next')).toBeFalsy()
+  expect(fp.has(data, 'second')).toBeFalsy()
+  expect(fp.has(data, 'first.last')).toBeFalsy()
+  expect(fp.has(data, 'first.second.next')).toBeFalsy()
 })
 
 it('get path in object', () => {
@@ -53,18 +53,18 @@ it('get path in object', () => {
     }
   }
 
-  expect(Path.get(data, 'first.second.what')).toBe('this')
-  expect(Path.get(data, 'first.second.next')).toBeTruthy()
-  expect(Path.get(data, 'first.second.last')).toBeFalsy()
+  expect(fp.get(data, 'first.second.what')).toBe('this')
+  expect(fp.get(data, 'first.second.next')).toBeTruthy()
+  expect(fp.get(data, 'first.second.last')).toBeFalsy()
 
-  expect(Path.get(data, 'simple')).toEqual({ lorem: false })
+  expect(fp.get(data, 'simple')).toEqual({ lorem: false })
 })
 
 it('require path', () => {
-  expect(Path.require(__dirname + '/fixtures/nested.childs.handler').call()).toBe('found nested')
-  expect(Path.require(__dirname + '/fixtures/example.handler').call()).toBe('found example')
-  expect(Path.require(__dirname + '/fixtures/flat').call()).toBe('found flat')
+  expect(fp.require(__dirname + '/fixtures/nested.childs.handler').call()).toBe('found nested')
+  expect(fp.require(__dirname + '/fixtures/example.handler').call()).toBe('found example')
+  expect(fp.require(__dirname + '/fixtures/flat').call()).toBe('found flat')
 
-  expect(() => typeof Path.require(__dirname + '/fixtures/example.handler.unknown')).toThrow()
-  expect(() => typeof Path.require(__dirname + '/fixtures/flat.unknown')).toThrow()
+  expect(() => typeof fp.require(__dirname + '/fixtures/example.handler.unknown')).toThrow()
+  expect(() => typeof fp.require(__dirname + '/fixtures/flat.unknown')).toThrow()
 })
